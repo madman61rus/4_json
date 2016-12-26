3a8fb8369f7974350707546bb25cb4c0fed8481c
madman61rus <rhjnhjtn3yjhe>
2016-12-27 01:33:34 +0300

    Попытка номер раз

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,20 @@
 import json
-
+import pprint
+import os
+import codecs
 
 def load_data(filepath):
-    pass
-
+    if not os.path.exists(filepath):
+        return None
+    with open(filepath, 'r', encoding='utf-8') as data_file:
+        data = json.load(data_file)
+    return data
 
 def pretty_print_json(data):
-    pass
+    print (json.dumps(data, indent=4, sort_keys=True, ensure_ascii=False))
 
 
 if __name__ == '__main__':
-    pass
+    file_path = input('Введите путь к файлу ')
+    json_data = load_data(file_path)
+    pretty_print_json(json_data)

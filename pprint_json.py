afa33410b1c0b3b24745da9a50b94145ef387ada
madman61rus <rhjnhjtn3yjhe>
2016-12-27 01:35:07 +0300

    Убрал data

--- pprint_json.py
+++ pprint_json.py
@@ -7,14 +7,14 @@ def load_data(filepath):
     if not os.path.exists(filepath):
         return None
     with open(filepath, 'r', encoding='utf-8') as data_file:
-        data = json.load(data_file)
-    return data
+        shops_list = json.load(data_file)
+    return shops_list
 
-def pretty_print_json(data):
-    print (json.dumps(data, indent=4, sort_keys=True, ensure_ascii=False))
+def pretty_print_json(shops):
+    print (json.dumps(shops, indent=4, sort_keys=True, ensure_ascii=False))
 
 
 if __name__ == '__main__':
     file_path = input('Введите путь к файлу ')
-    json_data = load_data(file_path)
-    pretty_print_json(json_data)
+    shops_list = load_data(file_path)
+    pretty_print_json(shops_list)

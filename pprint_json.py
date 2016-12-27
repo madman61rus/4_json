017d2f0f7617593c0aa0414510457c91a596d5e7
madman61rus <rhjnhjtn3yjhe>
2016-12-27 23:12:44 +0300

    - Убрал глобальные переменные - Убрал пробел между print и (

--- pprint_json.py
+++ pprint_json.py
@@ -7,14 +7,14 @@ def load_data(filepath):
     if not os.path.exists(filepath):
         return None
     with open(filepath, 'r', encoding='utf-8') as data_file:
-        shops_list = json.load(data_file)
-    return shops_list
+        shops = json.load(data_file)
+    return shops
 
 def pretty_print_json(shops):
-    print (json.dumps(shops, indent=4, sort_keys=True, ensure_ascii=False))
+    print(json.dumps(shops, indent=4, sort_keys=True, ensure_ascii=False))
 
 
 if __name__ == '__main__':
-    file_path = input('Введите путь к файлу ')
-    shops_list = load_data(file_path)
+    input_file_path = input('Введите путь к файлу ')
+    shops_list = load_data(input_file_path)
     pretty_print_json(shops_list)
